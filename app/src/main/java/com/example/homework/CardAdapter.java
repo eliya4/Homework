24d3a369d1ba68ffdcdf9e1242efc3b67e8f4e2c
eliya <eliyacaspi4@gmail.com>
2024-02-20 11:02:36 +0200

    שיעור בכיתה

--- app/src/main/java/com/example/homework/CardAdapter.java
+++ app/src/main/java/com/example/homework/CardAdapter.java
@@ -25,7 +25,7 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.MyViewHolder> 
     @Override
     public CardAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.row_layout,parent);
+        View view = inflater.inflate(R.layout.row_layout,parent,false);
         return new MyViewHolder(view);
     }
 
