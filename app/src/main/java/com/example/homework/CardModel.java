0dd32f259313aa6cfd2114fba98ca61b6a65c0ba
eliya <eliyacaspi4@gmail.com>
2024-03-02 19:54:21 +0200

    CardModel

--- app/src/main/java/com/example/homework/CardModel.java
+++ app/src/main/java/com/example/homework/CardModel.java
@@ -2,15 +2,20 @@ package com.example.homework;
 
 public class CardModel {
     String text;
+    String text2;
     int image;
 
     public CardModel(String text, int image) {
         this.text = text;
+        this.text2=text2;
         this.image = image;
     }
 
     public String getText() {
         return text;
+    }
+    public String getText2() {
+        return text2;
     }
 
     public int getImage() {
